5221de7fd167bc7cba67cc81e4c1f3ff1cbe72f0
bearmit <berenger@mit.edu>
2023-04-03 15:27:01 -0400

    fix: delete built file

--- scripts/typescript/afrl-to-new-api/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const wrong_aborted_script_1 = require("../src/scripts/wrong-aborted-script");
-(0, wrong_aborted_script_1.main)({ log: true });
