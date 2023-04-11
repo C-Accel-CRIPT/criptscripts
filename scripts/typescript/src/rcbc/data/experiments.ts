1cc6c3b02c4fae707e5a514bee747699d4353d6b
Berenger DALLE-CORT <berenger@mit.edu>
2023-04-11 14:04:38 -0400

    feat(rcbc): add computations
    Needs to be referenced by material.property[i].computation

--- scripts/typescript/src/rcbc/data/experiments.ts
+++ scripts/typescript/src/rcbc/data/experiments.ts
@@ -1,6 +1,7 @@
 import { IData, IExperiment, IProcess } from "@cript";
 import * as datasets from "./datasets";
 import * as processes from "./processes";
+import * as computations from "./computations";
 
 export const synth_mol1: Partial<IExperiment> = {
   name: "Synthesis of molecule 1 ((2,5-Di(2′-ethylhexyloxy)toluene))",
@@ -52,7 +53,7 @@ export const synth_polyphenylenevinylene: Partial<IExperiment> = {
     processes.acidified_water_and_stir,
     processes.collect_neutralize_product,
     processes.fractionate_with_chroma,
-  ] as IProcess[]
+  ] as IProcess[],
 };
 
 // Note: synth_xx are very similar in term of structure
@@ -60,64 +61,68 @@ export const synth_polyphenylenevinylene: Partial<IExperiment> = {
 export const synth_42: Partial<IExperiment> = {
   name: "Synthesis of PPVbPI-42",
   node: ["Experiment"],
-  data: [
-    datasets.gpc_pi_synth_42 as IData
-  ],
+  data: [datasets.gpc_pi_synth_42 as IData],
   process: [
     processes.anionic_polymerization_synth_42,
     processes.react_for_30mn_synth_42,
     processes.terminate_remaining_synth_42,
     processes.precipitate_with_methanol_synth_42,
-    processes.remove_homopolymer_with_synth_42
-  ] as IProcess[]
+    processes.remove_homopolymer_with_synth_42,
+  ] as IProcess[],
 };
 
 export const synth_59: Partial<IExperiment> = {
   name: "Synthesis of PPVbPI-59",
   node: ["Experiment"],
-  data: [
-    datasets.gpc_pi_synth_59 as IData
-  ],
+  data: [datasets.gpc_pi_synth_59 as IData],
   process: [
     processes.anionic_polymerization_synth_59,
     processes.react_for_30mn_synth_59,
     processes.terminate_remaining_synth_59,
     processes.precipitate_with_methanol_synth_59,
-    processes.remove_homopolymer_with_synth_59
-  ] as IProcess[]
+    processes.remove_homopolymer_with_synth_59,
+  ] as IProcess[],
 };
 
 export const synth_72: Partial<IExperiment> = {
   name: "Synthesis of PPVbPI-72",
   node: ["Experiment"],
-  data: [
-    datasets.gpc_pi_synth_72 as IData
-  ],
+  data: [datasets.gpc_pi_synth_72 as IData],
   process: [
     processes.anionic_polymerization_synth_72,
     processes.react_for_30mn_synth_72,
     processes.terminate_remaining_synth_72,
     processes.precipitate_with_methanol_synth_72,
-    processes.remove_homopolymer_with_synth_72
-  ] as IProcess[]
+    processes.remove_homopolymer_with_synth_72,
+  ] as IProcess[],
 };
 
 export const synth_89: Partial<IExperiment> = {
   name: "Synthesis of PPVbPI-89",
   node: ["Experiment"],
-  data: [
-    datasets.gpc_pi_synth_89 as IData
-  ],
+  data: [datasets.gpc_pi_synth_89 as IData],
   process: [
     processes.anionic_polymerization_synth_89,
     processes.react_for_30mn_synth_89,
     processes.terminate_remaining_synth_89,
     processes.precipitate_with_methanol_synth_89,
-    processes.remove_homopolymer_with_synth_89
-  ] as IProcess[]
+    processes.remove_homopolymer_with_synth_89,
+  ] as IProcess[],
 };
 
 export const phase_behavior: Partial<IExperiment> = {
   name: "Phase Behavior Study",
   node: ["Experiment"],
+  computation: [
+    
+    computations.analysis_PPVbPI_42,
+    computations.analysis_PPVbPI_59,
+    computations.analysis_PPVbPI_72,
+    computations.analysis_PPVbPI_89,
+
+    computations.peak_phase_id_PPVbPI_42,
+    computations.peak_phase_id_PPVbPI_59,
+    computations.peak_phase_id_PPVbPI_72,
+    computations.peak_phase_id_PPVbPI_89,
+  ],
 };
