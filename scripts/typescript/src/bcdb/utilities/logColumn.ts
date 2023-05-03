b89480fd6952b2f8ad0e1bb678806c9ec160f3af
Berenger DALLE-CORT <111717478+bearmit@users.noreply.github.com>
2023-05-03 09:49:42 -0400

    Revert "feat(typescript): add bcdb"

--- scripts/typescript/src/bcdb/utilities/logColumn.ts
+++
@@ -1,8 +0,0 @@
-import { Row, Column } from "../types";
-
-export function logColumn(rows: Row[], column: Column) {
-    for( let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-        const value = rows[rowIndex][column];
-        console.log(JSON.stringify(value, null, ''));
-    }
-}
